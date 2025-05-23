a5ae08a6d7787e280c8450a751502ce92c930c43
jcg1991 <leyendalerus1991@gmail.com>
2025-05-23 16:48:05 +0200

    Forzar redirección HTTPS desde main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,12 @@
+// Forzar redirección a HTTPS si se accede por HTTP
+if (window.location.protocol !== "https:") {
+  window.location.href =
+    "https://" +
+    window.location.hostname +
+    window.location.pathname +
+    window.location.search;
+}
+
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
