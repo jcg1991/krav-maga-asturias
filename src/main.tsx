5b6c586333e72d23e3a13c2ade6e3787ac40a877
jcg1991 <leyendalerus1991@gmail.com>
2025-05-23 17:25:47 +0200

    Eliminar redirección manual y activar redirección HTTPS desde Vercel

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,3 @@
-// Forzar redirección a HTTPS si se accede por HTTP
-if (window.location.protocol !== "https:") {
-  window.location.href =
-    "https://" +
-    window.location.hostname +
-    window.location.pathname +
-    window.location.search;
-}
-
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
